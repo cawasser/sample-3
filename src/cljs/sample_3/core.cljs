dd700e19c222efa32739f6c9c58c98bbd479d001
Chris Wasser <cawasser@mac.com>
2019-10-30 15:26:29 -0400

    second fetch-test commit

--- src/cljs/sample_3/core.cljs
+++ src/cljs/sample_3/core.cljs
@@ -97,6 +97,7 @@
 
 (defn about-page []
   [:section.section>div.container>div.content
+   [:div [:p "making another change to see how the commits flow back on merge/fetch"]]
    [:img {:src "/img/warning_clojure.png"}]])
 
 (defn home-page []
